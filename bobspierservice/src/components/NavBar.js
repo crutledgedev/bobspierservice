3fbc793bfd24628cc1a47ec0bd67a50d4b91f060
Chance Rutledge <chancerutledge2@gmail.com>
2020-06-20 13:05:25 -0700

    wire framed depenencies installed

--- bobspierservice/src/components/NavBar.js
+++ bobspierservice/src/components/NavBar.js
@@ -1,5 +1,26 @@
 import React from "react";
+import { Jumbotron, Container } from "reactstrap";
+import { Link } from "react-router-dom";
+import Navigation from "./Navigation";
 
-export default function NavBar() {
-  return <div>Navigation</div>;
-}
+const NavBar = (props) => {
+  return (
+    <div>
+      <Jumbotron fluid>
+        <Container fluid>
+          <h1 className="display-3">Bob's Pier Service</h1>
+
+          <p className="lead">
+            This is a modified jumbotron that occupies the entire horizontal
+            space of its parent.
+          </p>
+          {/* <Link to="/"> Home </Link>
+          <Link to="/gallery"> Gallery </Link> */}
+          <Navigation />
+        </Container>
+      </Jumbotron>
+    </div>
+  );
+};
+
+export default NavBar;
