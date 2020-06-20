0dbb17fc0a954a12c9d0e893ebd4f4ce1eedb526
Chance Rutledge <chancerutledge2@gmail.com>
2020-06-19 20:59:57 -0700

    basic routes added

--- bobspierservice/src/App.js
+++ bobspierservice/src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Link } from "react-router-dom";
+import Navbar from "./components/NavBar";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
+import Gallery from "./components/Gallery";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Route exact path="/">
+        <Body />
+      </Route>
+      <Route path="/gallery">
+        <Gallery />
+      </Route>
+      <Footer />
     </div>
   );
 }
